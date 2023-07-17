645770e34e582247725072524f9c32c58105847f
Agustin Mamani <agusdmu1999@gmail.com>
2023-07-17 14:59:40 -0500

    modelos funcionales

--- Registro/inscripciones/models.py
+++ Registro/inscripciones/models.py
@@ -32,3 +32,37 @@ class Academic(models.Model):
 
     def __str__(self):
         return self.academic_name + " " + self.department_id.department_name + " " + self.organization_id.organization_name
+
+class Course(models.Model):
+    course_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    course_name = models.CharField(max_length=100)
+    course_status = models.BooleanField(default=True)
+    course_created = models.DateTimeField(auto_now_add=True)
+    course_modified = models.DateTimeField(auto_now=True)
+
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    # ideal plan
+    # semester_id = models.ForeignKey('Semester', on_delete=models.CASCADE)
+    # plan_id = models.ForeignKey('Plan', on_delete=models.CASCADE)
+    # school_id = models.ForeignKey('School', on_delete=models.CASCADE)
+    # department_id = models.ForeignKey('Department', on_delete=models.CASCADE)
+    # faculty_id = models.ForeignKey('Faculty', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course_name + " " + self.organization_id.organization_name
+
+
+class User_Type(models.Model):
+    user_type_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_type_name = models.CharField(max_length=100)
+    user_type_status = models.BooleanField(default=True)
+    user_type_created = models.DateTimeField(auto_now_add=True)
+    user_type_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_type_name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_type_name'], name='unique_user_type_name')
+        ]
