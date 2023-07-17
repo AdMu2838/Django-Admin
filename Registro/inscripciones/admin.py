daf5784ebcb17a9485a493a7acdbdba720d68ddd
Agustin Mamani <agusdmu1999@gmail.com>
2023-07-17 15:04:37 -0500

    agregando mas modelos

--- Registro/inscripciones/admin.py
+++ Registro/inscripciones/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Organization,Academic,Department
+from .models import Organization,Academic,Department,User,User_Type,Teacher,Assignment\
+    ,Course
 # Register your models here.
 admin.site.register(Organization)
 admin.site.register(Academic)
 admin.site.register(Department)
+admin.site.register(User)
+admin.site.register(User_Type)
+admin.site.register(Teacher)
+admin.site.register(Assignment)
+admin.site.register(Course)
--- Registro/inscripciones/models.py
+++ Registro/inscripciones/models.py
@@ -66,3 +66,51 @@ class User_Type(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['user_type_name'], name='unique_user_type_name')
         ]
+
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_dni = models.IntegerField(unique=True)
+    user_password = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    user_last_name = models.CharField(max_length=100)
+    user_dob = models.DateField()
+    user_email = models.EmailField(max_length=254)
+    # add user phone
+    user_type_id = models.ForeignKey(User_Type, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_last_name + " " + self.user_name + "->" + self.user_type_id.user_type_name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_dni'], name='unique_user_dni'),
+            models.UniqueConstraint(fields=['user_email'], name='unique_user_email')
+        ]
+
+class Teacher(models.Model):
+    teacher_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    teacher_name = models.CharField(max_length=100)
+    teahcer_email = models.EmailField(max_length=254) # check this
+    teacher_status = models.BooleanField(default=True)
+    teacher_created = models.DateTimeField(auto_now_add=True)
+    teacher_modified = models.DateTimeField(auto_now=True)
+
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.teacher_name + " " + self.organization_id.organization_name
+class Assignment(models.Model):
+    assignment_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    assignment_status = models.BooleanField(default=True)
+    assignment_created = models.DateTimeField(auto_now_add=True)
+    assignment_modified = models.DateTimeField(auto_now=True)
+
+    academic_id = models.ForeignKey(Academic, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.academic_id) + " " + self.teacher_id.teacher_name + " " + self.course_id.course_name + " " + self.organization_id.organization_name
