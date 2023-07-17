6f36e32a93b339dfab4eb24406b7020f5e54b877
Agustin Mamani <agusdmu1999@gmail.com>
2023-07-17 15:33:51 -0500

    agrgando todos los modelos

--- Registro/inscripciones/admin.py
+++ Registro/inscripciones/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Organization,Academic,Department,User,User_Type,Teacher,Assignment\
-    ,Course
+from .models.Plan import Plan
+from .models.Student import Student
+from .models.Organization import Organization
+from .models.Academic import Academic
+from .models.Department import Department
+from .models.User import User
+from .models.User_Type import User_Type
+from .models.Teacher import Teacher
+from .models.Assignment import Assignment
+from .models.Course import Course
+from .models.School import School
+from .models.Enroll import Enroll
+from .models.Faculty import Faculty
+from .models.Group import Group
+from .models.Semester import Semester
 # Register your models here.
 admin.site.register(Organization)
 admin.site.register(Academic)
@@ -10,3 +23,10 @@ admin.site.register(User_Type)
 admin.site.register(Teacher)
 admin.site.register(Assignment)
 admin.site.register(Course)
+admin.site.register(School)
+admin.site.register(Enroll)
+admin.site.register(Group)
+admin.site.register(Faculty)
+admin.site.register(Student)
+admin.site.register(Plan)
+admin.site.register(Semester)
